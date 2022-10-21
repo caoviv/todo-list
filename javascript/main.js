bbb4c2bf786260da99980887776ddaa617c3e20c
VivianC <viviancao58@gmail.com>
2022-10-21 18:16:53 +1100

    create filter todo items function

--- javascript/main.js
+++ javascript/main.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 // Event Listener 
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 
 // Functions 
@@ -49,8 +51,8 @@ function deleteCheck(event) {
         const todo = item.parentElement;
         //Add animation
         todo.classList.add('fall');
-        todo.addEventListener('transitioned', function(){
-            todo.remove()
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
         })
     }
 
@@ -59,4 +61,29 @@ function deleteCheck(event) {
         const todo = item.parentElement;
         todo.classList.toggle("completed");
     }
+}
+
+function filterTodo(event) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo) {
+        switch(event.target.value) {
+            case 'all':
+                todo.style.display = 'flex';
+                break;
+            case 'completed':
+                if (todo.classList.contains('completed')){
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none';
+                }
+                break;
+            case 'uncompleted':
+                if (!todo.classList.contains('completed')){
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none';
+                }
+                break;            
+        } 
+    });
 }
