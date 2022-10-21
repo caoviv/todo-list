787973078af04fd7611e130e345417dde378a1e8
VivianC <viviancao58@gmail.com>
2022-10-21 13:56:10 +1100

    declare var selectors

---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+// selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.add-todo-input');
+const todoList = document.querySelector('.todo-list');
+
+// Event Listener 
+
+// Functions 
